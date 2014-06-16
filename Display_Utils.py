d5e0a814b3a39960786313d181d7d11d65ed4ee1
Josiah Seaman <josiah.seaman@newlinetechnicalinnovations.com>
2014-06-16 15:46:01 -0600

    More Interactive Experimentation.

--- Display_Utils.py
+++ Display_Utils.py
@@ -13,10 +13,17 @@ from PIL import Image
 
 # <codecell>
 
+from IPython.html.widgets import interact, interactive, fixed
+from IPython.html import widgets
+from IPython.display import clear_output, display, HTML
+
+# <codecell>
+
 class Pic():
-    def __init__(self, data, width=None, height=None):
+    def __init__(self, data, width=None, height=None, start = None):
         self.width = width if width else int(sqrt(len(data))) #TODO: 2D arrays
         self.height = height if height else int(ceil(len(data) / width))
+        self.start = start if start else 1
         self.data = data
         
     def set_width(self, width):
@@ -26,12 +33,25 @@ class Pic():
         
     def show(self):
         im = Image.new('L', (self.width, self.height))
-        im.putdata(self.data)
+        im.putdata(self.data[self.start-1:])
         imshow(im, cmap=cm.Greys_r, interpolation='nearest')
         
     def __repr__(self):
         self.show()  #this actually get the graph out there
         return str((self.width, self.height))
+    
+    def update(self, sequence='', width=None, start=None): # for use with interact(update, ...)
+        if sequence: self.data = sequence
+        if width: self.set_width(width)
+        if start: self.start = start  
+        self.show() #display to screen
+        
+    def gui(self):
+        length = len(self.data)
+        return interactive(self.update, 
+                           sequence=self.data, 
+                           width=widgets.IntSliderWidget(min=1, max=length//2, value=self.width), 
+                           start=widgets.IntSliderWidget(min=0, max=length,    value=self.start))
 
 # <codecell>
 
@@ -44,16 +64,37 @@ class NucleotideDisplay(Pic):
         
     def show(self):
         im = Image.new('RGB', (self.width, self.height))
-        im.putdata(self.nucleotide_to_color(self.data))
+        im.putdata(self.nucleotide_to_color(self.data[self.start-1:]))
         imshow(im, interpolation='nearest')
         
     def nucleotide_to_color(self, sequence):
         #Pad with extra gray pixels?
         return [self.color_map[nucleotide] for nucleotide in sequence]
 
-# <headingcell level=5>
+# <headingcell level=1>
 
-# Use 2D histogram for Oligomer Display and Isochore Heatmap  http://matplotlib.org/api/pyplot_api.html#matplotlib.pyplot.hist2d
+# Interactive Section
+
+# <codecell>
+
+nd = NucleotideDisplay('AAGCTAGCGCAGACTCAGCACACACACGCTCGATGCACTGCATCCGATCGACGCCTAGCATCGACTCAGCAT')
+
+# <codecell>
+
+nd.gui()
+
+# <codecell>
+
+[widget for widget in dir(widgets) if widget.endswith('Widget')]
+
+# <codecell>
+
+
+# <codecell>
+
+
+# <codecell>
+
 
 # <headingcell level=3>
 
@@ -65,6 +106,7 @@ class NucleotideDisplay(Pic):
 # * threshholding boundaries between DNA elements
 #    * ...in Nucleotide Display when it's very zoomed out
 
-# <codecell>
+# <headingcell level=5>
 
+# Use 2D histogram for Oligomer Display and Isochore Heatmap  http://matplotlib.org/api/pyplot_api.html#matplotlib.pyplot.hist2d
 
--- Notebook_Experimentation.py
+++ Notebook_Experimentation.py
@@ -7,13 +7,14 @@
 
 # <codecell>
 
-%reload_ext Display_Utils
-from Display_Utils import Pic, NucleotideDisplay
+from IPython.html.widgets import interact, interactive, fixed
+from IPython.html import widgets
+from IPython.display import clear_output, display, HTML
 
 # <codecell>
 
 from PIL import Image
-im = Image.new('L', (4,10))
+im = Image.new('L', (4,4))
 im.putdata([1,2,3,5,4,6,7,2,3,0,4,4])
 imshow(im, cmap=cm.Greys_r, interpolation='nearest')
 
@@ -23,13 +24,14 @@ Pic([1,2,3,4,5,6], 2)
 
 # <codecell>
 
-ceil?
-
-# <codecell>
-
 im = Image.new('RGB', (2,2))
 im.putdata([(255,0,0), (0,255,0), (0,0,255), (0,0,0)])
 imshow(im, interpolation='nearest')
+
+# <codecell>
+
+%reload_ext Display_Utils
+from Display_Utils import Pic, NucleotideDisplay
 
 # <codecell>
 
@@ -38,17 +40,165 @@ nd
 
 # <codecell>
 
-from IPython.html.widgets import interact, interactive, fixed
-from IPython.html import widgets
-from IPython.display import clear_output, display, HTML
-
-def width_slide(Width):
-    nd.set_width(Width)
-    nd.show()
+interactive_nucleotide = interact(width_slide, Width=(1,40))
 
 # <codecell>
 
-interact(width_slide, Width=(1,40))
+
+# <codecell>
+
+interactive_nucleotide.widget
+
+# <codecell>
+
+widgets.IntTextWidget().keys
+
+# <codecell>
+
+int_range = widgets.IntSliderWidget()
+display(int_range)
+
+def on_value_change(name, value):
+    print(name, value)
+
+int_range.on_trait_change(on_value_change)
+
+# <codecell>
+
+int_range.max = 10
+
+# <codecell>
+
+def new_button(clicked):
+    button = widgets.ButtonWidget()
+    button.clicks = 0
+    clicked.clicks += 1
+    button.description = "%d" % clicked.clicks
+    display(button)
+    button.on_click(new_button)
+button = widgets.ButtonWidget(description = "Start")
+button.clicks = 0
+display(button)
+button.on_click(new_button)
+
+# <codecell>
+
+container = widgets.ContainerWidget()
+display(container)
+container.children = [widgets.IntSliderWidget()]
+
+# <codecell>
+
+container.children.append(widgets.IntProgressWidget())
+
+# <codecell>
+
+container.children
+
+# <codecell>
+
+form = widgets.ContainerWidget()
+first = widgets.TextWidget(description="First Name:")
+last = widgets.TextWidget(description="Last Name:")
+
+student = widgets.CheckboxWidget(description="Student:", value=False)
+school_info = widgets.ContainerWidget(visible=False, children=[
+    widgets.TextWidget(description="School:"),
+    widgets.IntTextWidget(description="Grade:", min=0, max=12)
+    ])
+
+pet = widgets.TextWidget(description="Pet's Name:")
+form.children = [first, last, student, school_info, pet]
+display(form)
+
+def on_student_toggle(name, value):
+    if value:
+        school_info.visible = True
+    else:
+        school_info.visible = False
+student.on_trait_change(on_student_toggle, 'value')
+
+# <codecell>
+
+label = widgets.LatexWidget()
+label.value = "$\\textbf{ALERT:} Hello World!$"
+container = widgets.ContainerWidget(children=[label])
+
+# # set_css used to set a single CSS attribute.
+# container.set_css('border', '3px solid black') # Border the container
+
+# # set_css used to set multiple CSS attributes.
+# container.set_css({'padding': '6px', # Add padding to the container
+#                    'background': 'yellow'}) # Fill the container yellow
+
+display(container)
+container.add_class('corner-all')
+label.add_class('alert')
+
+# <codecell>
+
+display(container)
+
+# <codecell>
+
+import time
+label = widgets.LatexWidget(value = "$\\textbf{ALERT:} Hello World!$")
+display(label)
+
+# Apply twitter bootstrap alert class to the label.
+label.add_class("alert")
+
+# Animate through additional bootstrap label styles 3 times
+additional_alert_styles = ['alert-error', 'alert-info', 'alert-success']
+for i in range(100 * len(additional_alert_styles)):
+    label.add_class(additional_alert_styles[i % 3])
+    label.remove_class(additional_alert_styles[(i-1) % 3])
+    time.sleep(1)
+
+# <codecell>
+
+import time
+label = widgets.LatexWidget(value = "$\\textbf{ALERT:} Hello World!$")
+display(label)
+
+# Apply twitter bootstrap alert class to the label.
+label.add_class("alert")
+
+# Animate through additional bootstrap label styles 3 times
+additional_alert_styles = ['alert-error', 'alert-info', 'alert-success']
+for i in range(3 * len(additional_alert_styles)):
+    label.add_class(additional_alert_styles[i % 3])
+    label.remove_class(additional_alert_styles[(i-1) % 3])
+    time.sleep(1)
+
+# <codecell>
+
+import time
+labe = widgets.LatexWidget(value = "$\\textbf{ALERT:} Hello World!$")
+display(labe)
+
+# Apply twitter bootstrap alert class to the label.
+labe.add_class("alert")
+
+# Animate through additional bootstrap label styles 3 times
+additional_alert_styles = ['alert-error', 'alert-info', 'alert-success']
+for i in range(3 * len(additional_alert_styles)):
+    labe.add_class(additional_alert_styles[i % 3])
+    labe.remove_class(additional_alert_styles[(i-1) % 3])
+    time.sleep(1)
+
+# <codecell>
+
+print('hi')
+
+# <codecell>
+
+import winsound
+def annoy():     
+    for i in range(1, 10):             
+        winsound.Beep(i * 100, 200)
+
+annoy()
 
 # <codecell>
 
